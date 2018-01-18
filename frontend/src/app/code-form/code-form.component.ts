0acbe68e61313a0f0289e6b2ba20c6dc4eacbc42
danielMensah <mensahezros@gmail.com>
2018-01-18 12:21:46 +0000

    Add form that sends code (#9)

---
+++ frontend/src/app/code-form/code-form.component.ts
@@ -0,0 +1,34 @@
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+@Component({
+  selector: 'app-code-form',
+  templateUrl: './code-form.component.html',
+  styleUrls: ['./code-form.component.css']
+})
+export class CodeFormComponent implements OnInit {
+
+  code:string;
+
+  constructor(private http: HttpClient) { }
+
+  ngOnInit() {
+    
+  }
+
+  sendCode(code: HTMLInputElement) : boolean {
+    const body = {"text": code.value};
+
+    console.log(`Sending body...`);
+    console.log(body);
+    this.http.post("https://httpbin.org/post",body).subscribe(data => {
+      console.log("Attempted to send...");
+      console.log("Response....");
+      console.log(data)
+    });
+
+    return false;
+
+  }
+
+}
